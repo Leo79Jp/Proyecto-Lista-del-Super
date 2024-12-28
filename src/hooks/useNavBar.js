04a699736e6a6fecb781f9d16790e21ef7aebb53
Leo79Jp <leojparodi@gmail.com>
2024-12-28 10:58:16 -0300

    Cambio de ruta

--- src/hooks/useNavBar.js
+++ src/hooks/useNavBar.js
@@ -9,7 +9,7 @@ const useNavBar = () => {
   });
 
   useEffect(() => {
-    fetch("../public/datos/usuarios.json")
+    fetch("https://vercel.com/leoparodis-projects/proyecto-lista-del-super/4eVt6fxzkiaEbWotCRTiLsovEi4Q/source?f=src%2Fpublic%2Fdatos%2Fusuarios.json")
       .then((response) => response.json())
       .then((datos) => {
         setUsuarios(datos);
